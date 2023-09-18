58d7c02494160e3b43aff4a3282ba7d66fa04301
João Vitor <jooojbr945@gmail.com>
2023-09-18 18:32:43 -0300

    Expotec

---
+++ index-prof.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <!-- Meta tags Obrigatórias -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" crossorigin="anonymous">
+    <title>FortecHub</title>
+  </head>
+  <body>
+  <script src="js/bootstrap.bundle.min.js"></script>
+
+
+  <!-- Navbar -->
+  <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #244393;">
+ 
+  <div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link"  style="color: white">Inicio</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" style="color: white">Adicionar Notas</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link"  style="color: white">Horários</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+<div class="conteiner">
+    <div class="row">
+        <div class="col mt-5">
+          
+           <!--
+           // include("config.php");
+           // switch(@$_REQUEST ["page"]){
+           //     case "novo":
+           //           include("novo.php");
+           //     break;
+           //     case "livros":
+           //          include("livros.php");
+           //     break;
+           //     case "salvar":
+           //          include("salvar.php");  
+           //     break;
+           //     case "editar":
+           //       include("editar.php");  
+           //     break;
+           //     case "editar-conta":
+           //       include("editar-conta.php");  
+           //     break;
+           //     case "login":
+           //       include("login.php");  
+           //     break;
+           //     case "dados":
+           //       include("dados.php");  
+           //     break;
+           //     case "usuarios":
+           //       include("contas.php");  
+           //     break;
+           //     default:
+           //          print "<center><h1>Bem Vindo!<h1>";
+           // }
+                
+          -->
+        </div>
+    </div>
+</div>
+
+
+           
+<style>
+          html,body{
+
+                  width:100vw;
+                  height:100vh;
+                  overflow-x:hidden;
+                  background-image:; 
+                  background-size: 100% 100%;
+                  background-attachment: fixed;
+                  background-repeat: no-repeat;
+
+          }
+          .conteiner h1{
+                  
+          }
+          nav a:hover{
+
+                background-color: #324F9A!important;
+                border-radius: 9px;
+            }
+          .left{
+                position: absolute;
+                top: 55px;
+                width: 20%;
+                height: 91%;
+                overflow: hidden;
+
+          }
+   
+
+</style>
+    <script src="js/bootstrap.min.js"  crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ login.php
@@ -0,0 +1,80 @@
+<link rel="stylesheet" href="css/bootstrap.min.css" crossorigin="anonymous">
+<div class="container">
+    <div class="box">
+    <form  method="POST">
+    <center><h1>Login</h1></center>
+        <input type="hidden" name="acao" value="conta">  
+        <div class="mb-3"> 
+            
+            <input type="text" name="nome_us" class="form-control" required placeholder="Nome">
+        </div>  
+        <div class="mb-3">
+            
+            <input type="password" name="senha" class="form-control"required placeholder="Senha">
+        </div>
+        <center>
+        <div class="mb-3">
+            <button type="submit" class="btn ">Enviar</button>
+        </div>
+        </center>
+    </form>
+    </div>
+</div>
+
+<style>
+        body{
+                background-image: url(img/bg.png);
+
+
+
+        }
+     .container{
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                width: 100%;
+                height: 70vh;
+        }
+        .box{
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                text-align: left;
+                align-items: center;
+                width: 420px;
+                height: 420px;
+                padding: 50px;
+                border-radius: 60px;
+                text-align: center;
+                background-color: white;
+                
+                
+
+                
+        }
+        .box h1{
+                margin-bottom: 20px;
+
+        }
+        .mb-3 input{
+            
+            background-color: white;
+            width: 100%;
+            border-style: hidden;
+            box-shadow: 0px 1px 4px grey;
+            margin-bottom: 26px;
+        
+        }
+        .mb-3 button{
+            background-color: #244393;
+            color: white;
+            width: 60%;
+            border-radius: 10px;
+        }
+        .mb-3 button:hover{
+            
+            background-color: rgb(217, 145, 0);
+        }
+        
+
+</style>

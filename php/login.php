ecf844ad9fc02e207dce10e422687bb06cc5260a
Bruno Teles <109414743+BrunoTeless@users.noreply.github.com>
2023-09-19 20:07:49 -0300

    login do usuario com verificação de senha e usuario

---
+++ php/login.php
@@ -0,0 +1,52 @@
+<?php
+
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    include_once "./conexao.php";
+    
+    
+    $user = $_POST["nome_us"];
+    $senha = $_POST["senha_us"];
+
+
+    $query = "SELECT id, senha FROM alunos WHERE user = ?";
+
+
+    $stmt = $mysqli->prepare($query);
+
+    if ($stmt) {
+
+        $stmt->bind_param("s", $user);
+        $stmt->execute();
+
+
+        $stmt->bind_result($aluno_id, $senha_hash);
+
+
+        if ($stmt->fetch()) {
+
+            if (password_verify($senha, $senha_hash)) {
+
+                echo '<script>console.log("Login bem-sucedido. Bem-vindo, Aluno!")</script>';
+
+            } else {
+                echo '<script>console.log("Senha incorreta. Por favor, tente novamente.")</script>';//CONSOLE NÃO MUDA AKI IDIOTA
+
+                echo "Senha incorreta. Por favor, tente novamente.";
+                echo '<script>console.log("")</script>';
+            }
+        } else {
+
+            echo "Usuário não encontrado. Por favor, verifique o nome de usuário.";
+            echo '<script>console.log("Usuário não encontrado. Por favor, verifique o nome de usuário.")</script>';//CONSOLE NÃO MUDA AKI JOÃO BURRO
+        }
+
+        $stmt->close();
+    } else {
+        echo '<script>console.log("Erro na preparação da consulta.")</script>';
+    }
+
+    $mysqli->close();
+}
+?>

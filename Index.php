89a0b2fe0ed6c5126837e0bf98665f433eee72ec
João Vitor <jooojbr945@gmail.com>
2023-09-27 16:50:12 -0300

    Alteração na tela de Inicio

--- Index.php
+++ Index.php
@@ -3,7 +3,7 @@
   <style>
     
     #prof img{
-          position: absolute;
+          
           background-color: #244393;
           left: 40%;
           top: 20%;
@@ -16,15 +16,13 @@
           box-shadow: 0px 5px 10px black;
           overflow: hidden;
     }
-    #prof :hover{
+    #prof img:hover{
           background-color: #192f69;
 
     }
     #alunos img{
-          position: absolute;
+          
           background-color: #244393;
-          left: 15%;
-          top: 20%;
           width: 300px;
           height: 300px;
           border-style: solid;
@@ -34,15 +32,13 @@
           box-shadow: 0px 5px 10px black;
           overflow: hidden;
     }
-    #alunos :hover{
+    #alunos img:hover{
           background-color: #192f69;
 
     }
     #adm img{
-          position: absolute;
+         
           background-color: #244393;
-          left: 65%;
-          top: 20%;
           width: 300px;
           height: 300px;
           border-style: solid;
@@ -52,46 +48,69 @@
           box-shadow: 0px 5px 10px black;
           overflow: hidden;
     }
-    #adm :hover{
+    #adm img:hover{
           background-color: #192f69;
 
     }
-    #text h1{
-          position: absolute;
-          left: 35%;
-          top: 10%;
-
+    .vertical-align {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            min-height: 100vh;
+        }
+    .container{
+            position: absolute;
+            top: -10%;
+            left: 20%;
     }
+    .text-center h1{
+            font-size: 20px;
+    }
+
 </style>
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>FortecHub</title>
+    <link rel="stylesheet" href="./global/css/media.css" />
     <link rel="stylesheet" href="./global/css/bootstrap.min.css" crossorigin="anonymous">
+    <title>FortecHub</title>
   </head>
   <body>
-      <div id="text">
-        <center>
-        <h1> O que você deseja acessar?</h1>
-  </center>
+  
+  
+ 
+  <div class="container">
+        
+        
+        
+    <div class="row vertical-align">
+
+      <div id="prof"  class="col-md-4">
+        <div class="text-center">
+          <a href="./Global/login/loginProfessores.php">
+            <img src="./global/img/proficon.png"  class="img-fluid" alt="Imagem 1">
+          </a>
+          <h1>Hub dos Professores</h1>
+        </div>
+      </div>
+      <div id="alunos" class="col-md-4">
+        <div class="text-center">
+          <a href="./Global/login/loginAlunos.php">
+            <img src="./global/img/alunoicon.png" class="img-fluid" alt="Imagem 2">
+          </a>
+          <h1>Hub dos Alunos</h1>
+        </div>
+      </div>
+      <div id="adm" class="col-md-4">
+        <div class="text-center">
+          <a href="./Global/login/loginAdmin.php">
+            <img src="./global/img/cordicon.png"   class="img-fluid" alt="Imagem 3">
+          </a>
+          <h1>Hub dos Coordenadores</h1>
+        </div>
+      </div>
+    </div>
   </div>
-      <div id="prof">
-        <a href="./Global/login/loginProfessores.php">
-          <img src="./global/img/proficon.png">
-        </a>
-      </div>
-      <div id="alunos">
-        <a href="./Global/login/loginAlunos.php">
-          <img src="./global/img/alunoicon.png">
-        </a>
-      </div>
-      <div id="adm">
-        <a href="./Global/login/loginAdmin.php">
-        <img src="./global/img/cordicon.png">
-        </a>
-      </div>
-
-
     <?php
     session_start();
     session_destroy();

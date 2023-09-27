55b034c55ea32f946716f21435b8ff25baf61548
Bruno Teles <109414743+BrunoTeless@users.noreply.github.com>
2023-09-27 17:24:59 -0300

    tela de avisos aluno

--- Global/Alunos/Avisos.php
+++ Global/Alunos/Avisos.php
@@ -37,6 +37,59 @@
     <div class="row">
         <div class="col mt-5">
         
+<?php
+session_start();
+$host = "localhost";
+$username = "root";
+$password = "";
+$database = "expotec_db";
+
+$conn = mysqli_connect($host, $username, $password, $database);
+
+if (!$conn) {
+    die("Erro na conexão com o banco de dados: " . mysqli_connect_error());
+}
+
+$turma_id = $_SESSION['turma_id'];
+
+$sql_avisos = "SELECT * FROM avisos WHERE turma_id = '$turma_id' ORDER BY data_aviso DESC";
+
+$result_avisos = mysqli_query($conn, $sql_avisos);
+
+
+
+if (!$result_avisos) {
+    die("Erro ao consultar os avisos da turma: " . mysqli_error($conn));
+}
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Avisos</title>
+</head>
+<body>
+    <h1>TODOS OS AVISOS</h1><p>
+    
+    <?php
+if (mysqli_num_rows($result_avisos) > 0) {
+  while ($row_aviso = mysqli_fetch_assoc($result_avisos)) {
+      $titulo = $row_aviso['titulo'];
+      $data_aviso = $row_aviso['data_aviso'];
+      $aviso = $row_aviso['aviso'];
+   
+      echo "<h2>$titulo</h2>";
+      echo "<strong>  Data: </strong> $data_aviso</p>";
+      echo "<p>$aviso</p>";
+  }
+    }
+ else {
+    echo "<p>Nenhum aviso disponível para esta turma.</p>";
+}
+    
+mysqli_close($conn);
+?>
         </div>
     </div>
 </div>
@@ -88,3 +141,4 @@
     <script src="js/bootstrap.min.js"  crossorigin="anonymous"></script>
   </body>
 </html>
+

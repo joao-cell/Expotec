63ddc6641042d989422cfd886ef8f75367a0304a
Bruno Teles <109414743+BrunoTeless@users.noreply.github.com>
2023-10-01 15:32:05 -0300

    navbar
    adição combobox navbar

--- Global/Admin/Avisos.php
+++ Global/Admin/Avisos.php
@@ -1,241 +1,109 @@
 <?php
-// Inicialização da sessão
+
 session_set_cookie_params(0);
 session_start();
-
-// Verifique se o usuário está autenticado
 if (empty($_SESSION['user'])) {
-    header('Location: ../login/logout.html');
-    exit();
+    header('Location: ../login/loginAdmin.php'); 
+    exit(); 
 }
 
-// Verifique se o botão "Sair" foi pressionado
 if (isset($_POST['sair'])) {
-    session_destroy();
-    header('Location: ../../Index.php'); 
-    exit();
-}
-// Conexão com o banco de dados (substitua com suas informações de conexão)
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "expotec_db";
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Verifique a conexão
-if ($conn->connect_error) {
-    die("Conexão com o banco de dados falhou: " . $conn->connect_error);
+  session_destroy();
+  header('Location: ../../Index.php'); 
 }
 
-// Consulta para recuperar as salas
-$sql = "SELECT id, nome FROM turmas";
-$result = $conn->query($sql);
 ?>
-
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-<style>
-    nav a:hover{
-        background-color: #324F9A!important;
-        border-radius: 9px;
-    }
-    #logout button:hover{
-            background-color:  #324f9a ;
-            border-radius: 4px;
-            
-}
-#logout button{
-  border-radius: 4px;
-  background-color: #244393 ;
-  color: white;
-}
-#logout{
-  height: 39px;
-
-}
-.row{
-                  position: flex;
-                  left: 50%;
-          }
-          .conteiner h1{
-                  text-align: left;
-                  font-size: 28px;
-                  
-          }
-          
-          .conteiner{
-                  position: absolute;
-                  left: 5%;
-                  top: 20%;
-                  border-style: none ;
-                  padding-bottom: 25px;
-                  padding-left: 15px;
-                  padding-right: 15px;
-                  box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 1px 3px 1px;
-                  border-radius: 9px;
-                  width: 25%;
-                }
-                .container{
-                  position: absolute;
-                  left: 31%;    
-                  top: 20%;
-                  border-style: none ;
-                  padding-bottom: 25px;
-                  padding-left: 15px;
-                  padding-right: 5px;
-                  box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 1px 3px 1px;
-                  border-radius: 9px;
-                  width: 25%;
-                }
-          nav a:hover{
-
-                background-color: #324F9A!important;
-                border-radius: 9px;
-            }
-          .left{
-                position: absolute;
-                top: 55px;
-                width: 20%;
-                height: 91%;
-                overflow: hidden;
-
-          }
-          #texto{
-                padding: 10px;
-
-
-        }
-          #texto h1{
-
-
-
-          }
-          #texto h2{
-                color: gray;
-                font-size: 18px;
-                
-
-        }
-        .col button{
-               
-                
-                background-color: #244393;
-                color: white;
-                width: 50%;
-                border-radius: 10px;
-                margin-bottom: 20px;
-
-        }
-        .col button:hover {
-              background-color: #192f69;
-        }
-        .col input{
-                width: 100%;
-                border-style: none;
-                border-radius: 9px;
-                box-shadow: rgba(0, 0, 0, 0.06) 0px 2px 4px 0px inset;
-                background-color: #F3F5F5;
-                font-size: 22px;
-        }
-        .col textarea{
-                position: flex;
-                resize: none;
-                width: 100%;
-                border-style: none;
-                
-                border-radius: 9px;
-                height: 20vh;
-                box-shadow: rgba(0, 0, 0, 0.06) 0px 2px 4px 0px inset;
-                background-color: #F3F5F5;
-        }
-        .col select{
-                padding: 1px;
-                border-radius: 1px;
-                border-style: none;
-                box-shadow: 0px 1px 4px grey;
-                color: gray;
-        }
-        #delete{
-            background-color: #F72427;
-            width: 7%;
-            
-        }
-        #delete:hover{
-            background-color: #aa0000;
-        }
-        #alterar{
-            background-color: #1963f7;
-            width: 7%;
-
-        }
-        #alterar:hover{
-            background-color: #164dbf;
-
-        }
-</style>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <!-- Inclua o Bootstrap CSS -->
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 
-<!-- Inclua o jQuery -->
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <!-- Inclua o jQuery -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
 
-<!-- Inclua o Popper.js -->
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+    <!-- Inclua o Popper.js -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
 
-<!-- Inclua o Bootstrap JS -->
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <!-- Inclua o Bootstrap JS -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
     
     <link rel="stylesheet" href="../css/bootstrap.min.css" crossorigin="anonymous">
     <title>FortecHub</title>
+    <style>
+        nav a:hover {
+            background-color: #324F9A!important;
+            border-radius: 9px;
+        }
+
+        #logout button:hover {
+            background-color: #324f9a;
+            border-radius: 4px;
+        }
+
+        #logout button {
+            border-radius: 4px;
+            background-color: #244393;
+            color: white;
+        }
+
+        #logout {
+            height: 39px;
+        }
+
+        .H1 {
+
+        }
+    </style>
 </head>
 <body>
-<script src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
 
-
-<nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #244393;">
-    <div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item">
-                <a class="nav-link" style="color: white" href="../admin/index.php">Inicio</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" style="color: white" href="../Admin/Cadastro.php">Cadastro</a>
-            </li>
-            <li class="nav-item">
-            <div class="dropdown nav-item">
-  <a class="nav-link dropdown-toggle" style="color: white" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-    Notas
-  </a>
-  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-    <a class="dropdown-item" href="../Admin/notas.php">Ver Notas</a>
-    <a class="dropdown-item" href="../Admin/InserirNotas.php">Inserir Nota</a>
-  </div>
-</div>
-
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" style="color: white" href="../admin/avisos.php">Avisos</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" style="color: white" href="../admin/horarios.php">Horários</a>
-            </li>
-        </ul>
-        <div id="logout">
-        <form method="post">
-          <div><button type="submit" name="sair" class="btn">Sair</button></div>
-        </form>
-    </div>
-    </div>
-</nav>
+    <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #244393;">
+        <div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" style="color: white" href="../admin/index.php">Inicio</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" style="color: white" href="../Admin/Cadastro.php">Cadastro</a>
+                </li>
+                <li class="nav-item">
+                    <div class="dropdown nav-item">
+                        <a class="nav-link dropdown-toggle" style="color: white" type="button" id="dropdownMenuNotas" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Notas
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenuNotas">
+                            <a class="dropdown-item" href="../Admin/Notas.php">Ver Notas</a>
+                            <a class="dropdown-item" href="../Admin/InserirNotas.php">Inserir Nota</a>
+                        </div>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" style="color: white" href="../admin/avisos.php">Avisos</a>
+                </li>
+                <li class="nav-item">
+                    <div class="dropdown nav-item">
+                        <a class="nav-link dropdown-toggle" style="color: white" type="button" id="dropdownMenuHorarios" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Horarios
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenuHorarios">
+                            <a class="dropdown-item" href="../Admin/horarios.php">Ver Horarios</a>
+                            <a class="dropdown-item" href="../Admin/formHorario.php">Inserir Horario</a>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+            <div id="logout">
+                <form method="post">
+                    <div><button type="submit" name="sair" class="btn">Sair</button></div>
+                </form>
+            </div>
+        </div>
+    </nav>
 <div id="texto">
     <center>
         <h1>Página de Avisos</h1>
--- Global/Admin/notas.php
+++ Global/Admin/notas.php
@@ -1,8 +1,9 @@
-<?php 
+<?php
+
 session_set_cookie_params(0);
 session_start();
 if (empty($_SESSION['user'])) {
-    header('Location: ../login/loginAlunos.php'); 
+    header('Location: ../login/loginAdmin.php'); 
     exit(); 
 }
 
@@ -12,118 +13,97 @@ if (isset($_POST['sair'])) {
 }
 
 ?>
-
-<style>
-    html,body{
-        width:100vw;
-        height:100vh;
-        overflow-x:hidden;
-        background-image:;
-        background-size: 100% 100%;
-        background-attachment: fixed;
-        background-repeat: no-repeat;
-    }
-    .conteiner h1{
-
-    }
-    nav a:hover{
-        background-color: #324F9A!important;
-        border-radius: 9px;
-    }
-
-    .left{
-        position: absolute;
-        top: 55px;
-        width: 20%;
-        height: 91%;
-        overflow: hidden;
-    }
-
-    #texto h1{
-
-    }
-
-    #texto h2{
-        color: gray;
-        font-size: 18px;
-    }
-    #logout button:hover{
-            background-color:  #324f9a ;
-            border-radius: 4px;
-            
-        }
-        #logout button{
-          border-radius: 4px;
-          background-color: #244393 ;
-          color: white;
-        }
-        #logout{
-          height: 39px;
-
-        }
-
-</style>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <!-- Inclua o Bootstrap CSS -->
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 
-<!-- Inclua o jQuery -->
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <!-- Inclua o jQuery -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
 
-<!-- Inclua o Popper.js -->
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+    <!-- Inclua o Popper.js -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
 
-<!-- Inclua o Bootstrap JS -->
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <!-- Inclua o Bootstrap JS -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
     
     <link rel="stylesheet" href="../css/bootstrap.min.css" crossorigin="anonymous">
     <title>FortecHub</title>
+    <style>
+        nav a:hover {
+            background-color: #324F9A!important;
+            border-radius: 9px;
+        }
+
+        #logout button:hover {
+            background-color: #324f9a;
+            border-radius: 4px;
+        }
+
+        #logout button {
+            border-radius: 4px;
+            background-color: #244393;
+            color: white;
+        }
+
+        #logout {
+            height: 39px;
+        }
+
+        .H1 {
+
+        }
+    </style>
 </head>
 <body>
-<script src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
 
-
-<nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #244393;">
-    <div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item">
-                <a class="nav-link" style="color: white" href="../admin/index.php">Inicio</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" style="color: white" href="../Admin/Cadastro.php">Cadastro</a>
-            </li>
-            <li class="nav-item">
-            <div class="dropdown nav-item">
-  <a class="nav-link dropdown-toggle" style="color: white" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-    Notas
-  </a>
-  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-    <a class="dropdown-item" href="../Admin/Notas.php">Ver Notas</a>
-    <a class="dropdown-item" href="../Admin/InserirNotas.php">Inserir Nota</a>
-  </div>
-</div>
-
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" style="color: white" href="../admin/avisos.php">Avisos</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" style="color: white" href="../admin/horarios.php">Horários</a>
-            </li>
-        </ul>
-        <div id="logout">
-        <form method="post">
-          <div><button type="submit" name="sair" class="btn">Sair</button></div>
-        </form>
-    </div>
-    </div>
-</nav>
+    <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #244393;">
+        <div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" style="color: white" href="../admin/index.php">Inicio</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" style="color: white" href="../Admin/Cadastro.php">Cadastro</a>
+                </li>
+                <li class="nav-item">
+                    <div class="dropdown nav-item">
+                        <a class="nav-link dropdown-toggle" style="color: white" type="button" id="dropdownMenuNotas" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Notas
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenuNotas">
+                            <a class="dropdown-item" href="../Admin/Notas.php">Ver Notas</a>
+                            <a class="dropdown-item" href="../Admin/InserirNotas.php">Inserir Nota</a>
+                        </div>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" style="color: white" href="../admin/avisos.php">Avisos</a>
+                </li>
+                <li class="nav-item">
+                    <div class="dropdown nav-item">
+                        <a class="nav-link dropdown-toggle" style="color: white" type="button" id="dropdownMenuHorarios" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Horarios
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenuHorarios">
+                            <a class="dropdown-item" href="../Admin/horarios.php">Ver Horarios</a>
+                            <a class="dropdown-item" href="../Admin/formHorario.php">Inserir Horario</a>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+            <div id="logout">
+                <form method="post">
+                    <div><button type="submit" name="sair" class="btn">Sair</button></div>
+                </form>
+            </div>
+        </div>
+    </nav>
 
 <div class="conteiner">
     <div class="row">
